2a682f8ce7127ddb789e0c4deb9d4351cf4eaa83
‘baolin <3071279398@qq.com>
2022-04-28 11:22:34 +0800

    feat(first commit): vue3+ts

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'aa'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+
+export default store
